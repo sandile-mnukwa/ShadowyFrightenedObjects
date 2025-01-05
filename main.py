a8903138230979dffa34dac310672e8b5b5f6937
Sandile Mnukwa <sandile.mnukwa@gmail.com>
2025-01-05 06:23:55 +0000

    First Game

---
+++ main.py
@@ -0,0 +1,45 @@
+print(r'''
+*******************************************************************************
+          |                   |                  |                     |
+ _________|________________.=""_;=.______________|_____________________|_______
+|                   |  ,-"_,=""     `"=.|                  |
+|___________________|__"=._o`"-._        `"=.______________|___________________
+          |                `"=._o`"=._      _`"=._                     |
+ _________|_____________________:=._o "=._."_.-="'"=.__________________|_______
+|                   |    __.--" , ; `"=._o." ,-"""-._ ".   |
+|___________________|_._"  ,. .` ` `` ,  `"-._"-._   ". '__|___________________
+          |           |o`"=._` , "` `; .". ,  "-._"-._; ;              |
+ _________|___________| ;`-.o`"=._; ." ` '`."\ ` . "-._ /_______________|_______
+|                   | |o ;    `"-.o`"=._``  '` " ,__.--o;   |
+|___________________|_| ;     (#) `-.o `"=.`_.--"_o.-; ;___|___________________
+____/______/______/___|o;._    "      `".o|o_.--"    ;o;____/______/______/____
+/______/______/______/_"=._o--._        ; | ;        ; ;/______/______/______/_
+____/______/______/______/__"=._o--._   ;o|o;     _._;o;____/______/______/____
+/______/______/______/______/____"=._o._; | ;_.--"o.--"_/______/______/______/_
+____/______/______/______/______/_____"=.o|o_.--""___/______/______/______/____
+/______/______/______/______/______/______/______/______/______/______/_____ /
+*******************************************************************************
+''')
+print("Welcome to Survivor Treasure Island.")
+print("Your mission is to find the Lost Treasure of Soweto.")
+choice1 = input('You\'re at a cross road, where do you go? \n'
+                      'Type "left" or "right". \n').lower()
+
+if choice1 == "left" or choice1 == "Left" or choice1 == "LEFT":
+    choice2 = input('You\'ve come to a lake of youth. There is an island in the middle of the lake.'
+                    'Type "wait" to wait for a boat. Type "swim" to swim across. \n').lower()
+    if choice2 == "wait" or choice2 == "Wait" or choice2 == "WAIT":
+        choice3 = input("You arrived at the island unharmed. There is a house with 3 doors. \n"
+                        "One red, one yellow and one blue. Which colour do you choose? \n").lower()
+        if choice3 == "red" or choice3 == "Red" or choice3 == "RED":
+            print("Burned by fire. Game Over.")
+        elif choice3 == "yellow" or choice3 == "Yellow" or choice3 == "YELLOW":
+            print("You found the treasure! You Win!")
+        elif choice3 == "blue" or choice3 == "Blue" or choice3 == "BLUE":
+            print("Hahaha Wrong, you got eaten by Beats. Game Over!")
+        else:
+            print("Game Over.")
+    else:
+        print("Attacked by an angry trout. Game Over.")
+else:
+    print("You Fell into a hole.Game Over. Sorry bye")
